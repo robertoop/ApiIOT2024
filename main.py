49df73173e72cd701100916306f7ecaac884fecf
robertoop <olmos.201020856@gmail.com>
2024-04-30 12:08:55 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from flask import Flask
 app=Flask(__name__)
 
 @app.route("/")
-def Hola_mundo():
-  return "Hola a todos"
+def Presentacion():
+  Mensaje="Esta es una api que te convierte Dias en Horas, Minutos o Segundos"
+  return Mensaje
 
-@app.route("/Adios")
-def Adios_mundo():
-  return "Adios :("
+@app.route("/Horas/<Dias>")
+def Conversor_Dias_Horas(Dias):
+  Resultado=int(Dias)*24
+  return f" {Dias} dias son {Resultado} horas"
 
 if __name__=="__main__":
   app.run()
